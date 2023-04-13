9c2f403ee3a625f5331a558c65cfcc4fc463993e
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-04-13 20:45:27 +0300

    update

---
+++ contacts.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+    // ...твій код
+  }
+  
+  function getContactById(contactId) {
+    // ...твій код
+  }
+  
+  function removeContact(contactId) {
+    // ...твій код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твій код
+  }
